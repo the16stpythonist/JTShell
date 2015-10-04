5f93974b551b94707ac3666251d7ba7de10f94e8
the16stpythonist <jonseb1998@gmail.com>
2015-10-04 15:00:36 +0200

    Initial commit

---
+++ test.py
@@ -0,0 +1,8 @@
+__author__ = 'Jonas'
+
+for a in range(10):
+    for b in range(10):
+        for c in range(10):
+            if ((a * 4) + (b * -16) + (c * 13)) == 15:
+                print([a, b, c])
+
